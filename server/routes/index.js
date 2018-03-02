5770fd294fcd551b77409f4c168b98174cc8a2ed
Merdoth <ucheya97@gmail.com>
2018-03-02 18:10:23 +0100

    feature(user-signin): - add user sign in controller - add user sign in route for easy access - add user sign in validation in the middleware folder
    [Delivers#155660252]

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,5 +3,6 @@ import userController from '../controllers/userControllers';
 import validators from '../middlewares/validators';
 const router = express.Router();
 router.post('/api/v1/signup', validators.validateSignUp, userController.signUp);
+router.post('/api/v1/signin', validators.validateSignIn, userController.signIn);
 
 export default router;
